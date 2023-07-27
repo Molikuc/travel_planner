b87adf984b39aa0ae540e991cfa54adaa5437d6c
Molikuc <ruiz.alexis1996@gmail.com>
2023-07-27 21:57:46 +0200

    Adding a Toaster on the user page

--- src/components/UserPlan.tsx
+++ src/components/UserPlan.tsx
@@ -1,14 +1,29 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useEffect } from "react";
 import Map from "./Map";
 import Days from "./Days";
+import { useToast } from "../hooks/use-toast";
+import { Button } from "./ui/Button";
+import { Toaster } from "./ui/Toaster";
 
 interface UserPlanProps {}
 
 const UserPlan: FC<UserPlanProps> = ({}) => {
+  const { toast } = useToast();
+
+  useEffect(() => {
+    toast({
+      title: "Pro Tips",
+      description: "Click on the activities to get the infos",
+    });
+  }, []);
+
   return (
     <div className="grid gap-12">
       <Map />
       <Days />
+      <Toaster />
     </div>
   );
 };
