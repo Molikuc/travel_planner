71c3833adbe7551b4990153bb0f39bfc283581d7
Molikuc <ruiz.alexis1996@gmail.com>
2023-07-30 14:55:04 +0200

    Adding the Auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,9 @@
-import { FC } from "react";
-import { Avatar, AvatarImage } from "./ui/Avatar";
 import { Command, CommandInput } from "./ui/Command";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./ui/DropdownMenu";
-import { Button } from "./ui/Button";
-import { FaUser } from "react-icons/fa";
-import { IoSettingsSharp, IoLogOut } from "react-icons/io5";
+import { buttonVariants } from "./ui/Button";
 import Link from "next/link";
+import UserAccount from "./UserAccount";
 
-interface NavbarProps {}
-
-const Navbar: FC<NavbarProps> = ({}) => {
+const Navbar = () => {
   return (
     <div className="fixed top-0 inset-x-0 bg-zinc-100 h-fit border-b py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
@@ -26,32 +13,8 @@ const Navbar: FC<NavbarProps> = ({}) => {
         <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
           <CommandInput placeholder="Type a place..." />
         </Command>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="gap-2 cursor-pointer">
-              <FaUser className="text-lg" />
-              Profile
-            </DropdownMenuItem>
-            <DropdownMenuItem className="gap-2 cursor-pointer">
-              <IoSettingsSharp className="text-lg" />
-              Settings
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="gap-2 cursor-pointer">
-              <IoLogOut className="text-xl" />
-              Log Out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+
+        <UserAccount />
       </div>
     </div>
   );
---
+++ src/components/UserAccount.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/DropdownMenu";
+import { Avatar, AvatarImage } from "./ui/Avatar";
+import { FaUser } from "react-icons/fa";
+import { IoSettingsSharp, IoLogOut } from "react-icons/io5";
+import Link from "next/link";
+import { buttonVariants } from "./ui/Button";
+
+const UserAccount = () => {
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <div>
+      {isLogged ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="cursor-pointer">
+              <AvatarImage src="https://github.com/shadcn.png" />
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="gap-2 cursor-pointer">
+              <FaUser className="text-lg" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-2 cursor-pointer">
+              <IoSettingsSharp className="text-lg" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="gap-2 cursor-pointer">
+              <IoLogOut className="text-xl" />
+              Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <Link href="/sign-in" className={buttonVariants()}>
+          Sign In
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default UserAccount;
